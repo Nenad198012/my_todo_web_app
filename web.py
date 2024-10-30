4cac134f59717290797b00e8cdbce95a0a4594f0
Nenad Andonov <nenad.andonov@yahoo.com>
2024-10-30 02:05:15 +0000

    Change the layout order

--- web.py
+++ web.py
@@ -15,6 +15,11 @@ st.title('My Todo App', )
 st.subheader('This is my todo app.')
 st.write('This app is to increase your productivity.')
 
+st.text_input(label='',
+              placeholder='Add new todo...',
+              on_change=add_todo,
+              key='new todo')
+
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -23,7 +28,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label='',
-              placeholder='Add new todo...',
-              on_change=add_todo,
-              key='new todo')
+
